5395a150a1b5f21a9fc783a27ffb347bab8959d6
PrathamSingh1 <prathamjii36@gmail.com>
2025-10-23 22:10:16 +0000

    Learn Streaming in Nextjs

---
+++ 5.Client Components vs Server Components/my-app/components/Likes.jsx
@@ -0,0 +1,8 @@
+
+export default async function Likes() {
+  await new Promise((resolve) => setTimeout(resolve, 6000))
+
+  return (
+    <div>2k Likes</div>
+  )
+}
---
+++ 5.Client Components vs Server Components/my-app/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div className="flex gap-5 items-center justify-center list-none">
+        <Link href={"/"}>
+        <li>Home</li>
+        </Link>
+        <Link href={"/about"}>
+        <li>About</li>
+        </Link>
+        <Link href={"/blogs"}>
+        <li>Blogs</li>
+        </Link>
+    </div>
+  )
+}
